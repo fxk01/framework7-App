ad516683ae3192f2b2e24c45f7a193b18e46f374
fxk01 <2416113928@qq.com>
2018-03-14 18:05:07 +0800

    logi

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -8,6 +8,11 @@ import Tool from '../utils/tool';
 import '../components/toast/toast.css'
 import '../components/toast/toast'
 import './login.less'
+import * as echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/line';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+import 'echarts/lib/component/toolbox';
 
 export default {
   init(page) {
@@ -16,9 +21,32 @@ export default {
     this.log();
   },
   log() {
+    let main = document.getElementById('main');
     $('.loginHref').on('click', function() {
       let toast = myApp.toast('账号密码错误！');
       toast.show();
-    })
+    });
+
+
+    echarts.init(main).setOption({
+      title: {text: 'Line Chart'},
+      tooltip: {},
+      toolbox: {
+        feature: {
+          dataView: {},
+          saveAsImage: {
+            pixelRatio: 2
+          },
+          restore: {}
+        }
+      },
+      xAxis: {},
+      yAxis: {},
+      series: [{
+        type: 'line',
+        smooth: true,
+        data: [[12, 5], [24, 20], [36, 36], [48, 10], [60, 10], [72, 20]]
+      }]
+    });
   }
 }

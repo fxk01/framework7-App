6099e57995f7e4957ed218186ca8e346c3008b7a
fxk01 <2416113928@qq.com>
2018-03-14 16:34:58 +0800

    弹框

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -3,13 +3,22 @@
  **/
 
 'use strict';
-import './login.less'
 import loginTpl from './login.tpl.html';
 import Tool from '../utils/tool';
+import '../components/toast/toast.css'
+import '../components/toast/toast'
+import './login.less'
 
 export default {
-  init(page){
+  init(page) {
     let loginTPl = Tool.renderTpl(loginTpl);
     $('.login-page').append($(loginTPl));
+    this.log();
   },
+  log() {
+    $('.loginHref').on('click', function() {
+      let toast = myApp.toast('账号密码错误！');
+      toast.show();
+    })
+  }
 }

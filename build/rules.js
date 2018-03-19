6b8c4beb9b4e7e501e10c1ff977721c9eddc7c8e
fxk01 <2416113928@qq.com>
2018-03-19 11:27:20 +0800

    修改

--- build/rules.js
+++ build/rules.js
@@ -17,47 +17,69 @@ module.exports = [
   },
   {
     test: /\.js$/,
-    use: 'babel-loader',
+    use: [
+      'babel-loader'
+    ],
     exclude: /node_modules/
   },
   {
     test: /\.html$/,
-    use: 'html-loader'
+    use: [
+      'html-loader'
+    ]
   },
   {
     test: /\.png$/,
-    use: 'url-loader?limit=8192&mimetype=image/png'
+    use: [
+      'url-loader?limit=8192&mimetype=image/png'
+    ]
   },
   {
     test: /\.jpe?g$/,
-    use: 'ur-loaderl?limit=8192&mimetype=image/jpg'
+    use: [
+      'ur-loaderl?limit=8192&mimetype=image/jpg'
+    ]
   },
   {
     test: /\.gif$/,
-    use: 'url-loader?limit=8192&mimetype=image/gif'
+    use: [
+      'url-loader?limit=8192&mimetype=image/gif'
+    ]
   },
   {
     test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-    use: 'url-loader?limit=8192&mimetype=image/svg+xml'
+    use: [
+      'url-loader?limit=8192&mimetype=image/svg+xml'
+    ]
   },
   {
     test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-    use: 'url-loader?limit=8192&mimetype=application/font-woff2'
+    use: [
+      'url-loader?limit=8192&mimetype=application/font-woff2'
+    ]
   },
   {
     test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-    use: 'url-loader?limit=8192&mimetype=application/font-woff'
+    use: [
+      'url-loader?limit=8192&mimetype=application/font-woff'
+    ]
   },
   {
     test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-    use: 'url-loader?limit=8192&mimetype=application/octet-stream'
+    use: [
+      'url-loader?limit=8192&mimetype=application/octet-stream'
+    ]
   },
   {
     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-    use: 'file-loader'
+    use: [
+      'file-loader'
+    ]
   },
   {
     test: /\.json$/,
-    use: 'json-loader'
+    use: [
+      'json-loader'
+    ]
   }
 ];

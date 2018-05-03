d91ea157a4f90a1501bd0ba303486883118bba10
fxk01 <2416113928@qq.com>
2018-05-03 11:36:14 +0800

    基金产品

--- src/app/main/main.js
+++ src/app/main/main.js
@@ -21,6 +21,7 @@ export default class Main extends widget {
     $('.main-page').append($(_mainTpl));
     this.ajaxMoney();
     this.assetsChart();
+    this.postNetValue();
 
     let ptrContent = $('.pull-to-refresh-content');
     myApp.initPullToRefresh(ptrContent);
@@ -97,6 +98,68 @@ export default class Main extends widget {
       series: [{
         name: '小张',
         data: [3, 4, 3, 5, 4, 10, 12]
+      }]
+    });
+  }
+  postNetValue() {
+    highCharts.chart('containerJzZs', {
+      chart: {
+        type: 'area'
+      },
+      title: {
+        text: ' '
+      },
+      subtitle: {
+        text: ' '
+      },
+      xAxis: {
+        allowDecimals: false,
+        labels: {
+          formatter: function () {
+            return this.value;
+          }
+        }
+      },
+      yAxis: {
+        title: {
+          text: ' '
+        },
+        labels: {
+          formatter: function () {
+            return this.value / 1000 + 'k';
+          }
+        }
+      },
+      tooltip: {
+        pointFormat: '{series.name} 制造 <b>{point.y:,.0f}</b>枚弹头'
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        area: {
+          pointStart: 1940,
+          marker: {
+            enabled: false,
+            symbol: 'circle',
+            radius: 2,
+            states: {
+              hover: {
+                enabled: true
+              }
+            }
+          }
+        }
+      },
+      series: [{
+        name: ' ',
+        data: [null, null, null, null, null, 6, 11, 32, 110, 235, 369, 640,
+          1005, 1436, 2063, 3057, 4618, 6444, 9822, 15468, 20434, 24126,
+          27387, 29459, 31056, 31982, 32040, 31233, 29224, 27342, 26662,
+          26956, 27912, 28999, 28965, 27826, 25579, 25722, 24826, 24605,
+          24304, 23464, 23708, 24099, 24357, 24237, 24401, 24344, 23586,
+          22380, 21004, 17287, 14747, 13076, 12555, 12144, 11009, 10950,
+          10871, 10824, 10577, 10527, 10475, 10421, 10358, 10295, 10104]
       }]
     });
   }

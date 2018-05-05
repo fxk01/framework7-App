d4193c35d13451e9dc396c1cbe3b8eaa7f5cbbe7
fxk01 <2416113928@qq.com>
2018-05-05 23:37:54 +0800

    登录

--- src/app/main/main.js
+++ src/app/main/main.js
@@ -5,6 +5,7 @@
 'use strict';
 import './main.less';
 import mainTpl from './main.tpl.html';
+import companyList from '../../components/company-list/company-list.html'
 import Tool from '../../utils/tool';
 import widget from '../../utils/widget'
 
@@ -15,9 +16,8 @@ export default class Main extends widget {
   init() {
     let _mainTpl = Tool.renderTpl(mainTpl);
     $('.main-page').append($(_mainTpl));
-
-    myApp.prompt('', '', function (value) {
-      myApp.alert('Your name is "' + value + '". You clicked Ok button');
-    });
+    myApp.prompt('', '');
+    $('.modal-text-input').attr('placeholder', '请输入公司名称');
+    $('.modal').append(companyList);
   }
 };
